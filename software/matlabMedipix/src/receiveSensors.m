89892cc15a1830c864cc9f4a1532d274486601a9
Tomáš Báča <klaxalk@gmail.com>
2015-07-16 13:45:03 +0200

    sensor re-typed to signed int

--- software/matlabMedipix/src/receiveSensors.m
+++ software/matlabMedipix/src/receiveSensors.m
@@ -2,10 +2,10 @@
 while (s.BytesAvailable <= 0)
 end
 
-IR = swapbytes(uint16(fread(s, 1, 'uint16')));
-TIR = swapbytes(uint16(fread(s, 1, 'uint16')));
-UV1 = swapbytes(uint16(fread(s, 1, 'uint16')));
-UV2 = swapbytes(uint16(fread(s, 1, 'uint16')));
+IR = swapbytes(int16(fread(s, 1, 'int16')));
+TIR = swapbytes(int16(fread(s, 1, 'int16')));
+UV1 = swapbytes(int16(fread(s, 1, 'int16')));
+UV2 = swapbytes(int16(fread(s, 1, 'int16')));
 
 fread(s, 1, 'int16');
 
