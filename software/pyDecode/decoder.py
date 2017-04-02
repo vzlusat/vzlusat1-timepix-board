62c91c6c18bc8ae92c12d4397184ad5a439466f7
Tomáš Báča <klaxalk@gmail.com>
2017-04-02 20:42:15 +0200

    autoinstalling libraries

--- software/pyDecode/decoder.py
+++ software/pyDecode/decoder.py
@@ -1,4 +1,16 @@
-import matplotlib
+def install_and_import(package):
+    import importlib
+    try:
+        importlib.import_module(package)
+    except ImportError:
+        import pip
+        pip.main(['install', package])
+    finally:
+        globals()[package] = importlib.import_module(package)
+
+
+install_and_import('matplotlib')
+
 matplotlib.use('TkAgg')
 
 from numpy import arange, sin, pi
